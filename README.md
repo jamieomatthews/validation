29a167ee44bc92d5ad7fc73489e0f7334f888ccc
Jamie Matthews <jmatthews08@gmail.com>
2014-04-30 22:43:20 -0400

    fundementally changed the way this works - added chaning

--- README.md
+++ README.md
@@ -2,13 +2,37 @@
 
 The idea behind this repo is to give some nice default validation handlers to complement the [Martini Bind Contrib Package](https://github.com/martini-contrib/binding).  
 
-I have mocked up some really common ones, but please send a Pull Request if you develop other general ones you want to share.
+Heres an example of the syntax we are going for: (thanks Matt for suggesting)
+```go
+func (bp *BlogPost) Validate(errors binding.Errors, req *http.Request) Errors {
+    v := validator.New(errors, req)
+
+    v.Validate(&bp.Title).TrimSpace().Length(10, 100).MustNotContain("foo")
+    v.Validate(&bp.Subtitle).TrimSpace().MaxLength(50)
+    v.Validate(&bp.Content).TrimSpace().MustContain("Go", "Classification", "Custom message")
+    v.Validate(&bp.Author.Email).EmailAddress()
+    v.Validate(req).HasHeader("X-Foo-Bar") // maybe not useful; but the idea is you can do validation on the request itself
+
+    // then any other custom validation steps you want to perform yourself,
+    // more specific to your application or handler usually
+
+    return v.Errors()
+}
+```
+
+My goal is to make this actually work with any web framework, as it doesnt only apply to Martini.  *For now* I have hard coded in the binding.Errors as the internal error structure, but I will swap this out ASAP for an interface structure that would let anyone utilize it.
 
 Here are some validations I have created so far:
 Validations to perform:
 
-1.  Minimum Length / Max Length
-2.  Matches Pattern (takes Regex)
+-  Minimum Length / Max Length
+-  Matches Pattern (takes Regex)
+-  Email (uses matches pattern)
+
+As well as some utilities, like
+- TrimSpace
+- Default (overrides default error)
+- Classify (sets classification)
 
 
 More that I want to add when I have time:
@@ -21,7 +45,9 @@ More that I want to add when I have time:
 -  Use matches pattern to do URL, and other pattern like examples
 
 
-For anyone taking a look, I still need to finish this up a bit, as well as re-write the error mapping method to comply with the new [error struct](https://github.com/martini-contrib/binding/issues/22).  Bare with me, this will be done soon. I'll then slowly start chipping away at the second list of validators
+
+
+This is **very** work-in-progress, so don't rely on the sytax too much yet. Hope to reach a stable point in a few days.
 
 Ideas inspired from the [jQuery validation plugin](http://jqueryvalidation.org/documentation/) as well as the way .NET MVC handles [model validation](http://www.asp.net/mvc/tutorials/mvc-4/getting-started-with-aspnet-mvc4/adding-validation-to-the-model).
 
--- validation.go
+++ validation.go
@@ -1,61 +1,135 @@
 package validation
 
 import (
+	"fmt"
 	"net/http"
 	"regexp"
+	"strings"
 
 	"github.com/martini-contrib/binding"
 )
 
 type Validation struct {
-	Errors  *binding.Errors
+	Errors  binding.Errors
 	Request *http.Request
-	Field   interface{} //a pointer to the passed in feild
-	Key     string      //string key pulled from the field
-	IsValid bool
 }
 
-func New(errors *binding.Errors, request *http.Request) *Validation {
+//represents one 'set' of validation errors.
+type Set struct {
+	Field          interface{} //a pointer to the passed in feild
+	Key            string      //string key pulled from the field
+	IsValid        bool
+	Classification string
+	Message        string
+	Validation     *Validation //for now, keep a reference to the validation to map errors back
+}
+
+// //a struct that maps errors.  errors can span multiple feilds,
+// //and each field can have mutliple errors
+// type Error struct {
+// 	FieldNames     []string // name(s) of the fields involved, if any
+// 	Classification string   // error type or category
+// 	Message        string   // human-readable or detailed message
+// }
+
+func New(errors binding.Errors, request *http.Request) *Validation {
 	return &Validation{Errors: errors, Request: request}
 }
 
-func (v *Validation) Validate(field interface{}, key string) *Validation {
-	v.Field = field
-	v.Key = key
-	v.IsValid = true //valid until proven otherwise
-	return v
+func (v *Validation) Validate(field interface{}, key string) *Set {
+	s := &Set{Field: field, Key: key, IsValid: true, Validation: v}
+
+	return s
 }
+
+func (v *Validation) AddError(err binding.Error) {
+	v.Errors = append(v.Errors, err)
+}
+
+// func (v *Validation) ToJson() []byte {
+// 	m := make(map[string][]string)
+// 	for err := range v.Errors {
+// 		m[err.FieldNames[0]] =
+// 	}
+// }
 
 // returns true if the validator has 1 or more errors
-func (v *Validation) HasErrors() bool {
-	return len(v.Errors.Fields) > 0
+func (s *Set) HasErrors() bool {
+	return len(s.Validation.Errors) > 0
 }
 
-func (v *Validation) MaxLength(n int, maxLength int, fieldName string) bool {
-	return v.validate(MaxLength{MaxLength: maxLength}, n, fieldName)
+func (s *Set) MaxLength(maxLength int) *Set {
+	return s.validate(MaxLength{MaxLength: maxLength}, s.Len())
 }
 
-func (v *Validation) MinLength(n int, minLength int, fieldName string) bool {
-	return v.validate(MinLength{MinLength: minLength}, n, fieldName)
+func (s *Set) MinLength(minLength int) *Set {
+	fmt.Println("Min Length: '", s.Field, "'")
+	return s.validate(MinLength{MinLength: minLength}, s.Len())
 }
 
-func (v *Validation) Match(strMatch string, regex *regexp.Regexp, fieldName string) bool {
-	return v.validate(Matches{Regex: regex}, strMatch, fieldName)
+func (s *Set) Match(strMatch string, regex *regexp.Regexp) *Set {
+	return s.validate(Matches{Regex: regex}, strMatch)
 }
 
-func (v *Validation) Email(email string, fieldName string) bool {
+func (s *Set) Email() *Set {
 	var emailPattern = regexp.MustCompile("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$")
-	return v.validate(Email{Matches{emailPattern}}, email, fieldName)
+	return s.validate(Email{Matches{emailPattern}}, s.toString())
+}
+
+func (s *Set) Classify(str string) *Set {
+	s.Classification = str
+	return s
+}
+
+func (s *Set) Default(message string) *Set {
+	s.Message = message
+	return s
+}
+func (s *Set) TrimSpace() *Set {
+	if str, ok := s.Field.(string); ok {
+		s.Field = strings.TrimSpace(str)
+	}
+	return s
+}
+
+func (s *Set) getMessage(val Validator) string {
+	if s.Message == "" {
+		return val.DefaultMessage()
+	}
+	return s.Message
+}
+
+func (s *Set) Len() int {
+	if str, ok := s.Field.(string); ok {
+		return len(str)
+	}
+	if array, ok := s.Field.([]interface{}); ok {
+		return len(array)
+	}
+	if m, ok := s.Field.(map[interface{}]interface{}); ok {
+		return len(m)
+	}
+	return 0
+}
+
+func (s *Set) toString() string {
+	if str, ok := s.Field.(string); ok {
+		return str
+	}
+	panic("This method requires a string value")
 }
 
 //runs the validation rule, returns true if the rule passed
-func (v *Validation) validate(validator Validator, obj interface{}, fieldName string) bool {
+func (s *Set) validate(validator Validator, obj interface{}) *Set {
 	//check if the rule is valid
 	if validator.IsValid(obj) {
-		return true
+		fmt.Println("Validated")
+		s.IsValid = true
+		return s
 	}
 
 	//else, add a new validation error
-	v.Errors.Fields[fieldName] = validator.DefaultMessage()
-	return false
+	s.Validation.AddError(binding.Error{FieldNames: []string{s.Key}, Classification: s.Classification, Message: s.getMessage(validator)})
+	s.IsValid = false
+	return s
 }
